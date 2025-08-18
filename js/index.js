5de212241de226f87f0c424aeba5e58ca2a3e69b
Sayed Zulfikar Mahmud <sayedzulfikar2019@gmail.com>
2025-08-18 22:55:52 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const menuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+menuButton.addEventListener('click', () => {
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+        menuIcon.classList.add('hidden');
+        closeIcon.classList.remove('hidden');
+    } else {
+        mobileMenu.classList.add('hidden');
+        menuIcon.classList.remove('hidden');
+        closeIcon.classList.add('hidden');
+    }
+});
